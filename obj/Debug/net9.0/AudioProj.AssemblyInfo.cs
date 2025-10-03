24ab49d0d8445577cd99c65fec536c8305243e93
Shaqoo <ShakirullahOhio@gmail.com>
2025-10-03 11:42:10 +0100

    Update AssemblyInfo with detailed informational version and cache inputs

--- obj/Debug/net9.0/AudioProj.AssemblyInfo.cs
+++ obj/Debug/net9.0/AudioProj.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AudioProj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71a938fea7309126911b950995a1e1c996a5131f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AudioProj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AudioProj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
